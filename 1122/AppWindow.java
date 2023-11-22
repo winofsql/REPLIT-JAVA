4ee59c50087d588a7929f205fceaf405e187bce2
sworc <lightbox@nifty.com>
2023-11-22 11:02:37 +0900

    Add files via upload

--- 1122/AppWindow.java
+++ 1122/AppWindow.java
@@ -1,10 +1,15 @@
 package action;
 
+import java.sql.*;
 import javax.swing.*;
 import java.awt.*;
 import java.io.*;
 
 public class AppWindow extends BaseWindow {
+
+    private Connection conn = null;
+    private Statement stmt = null;
+    private ResultSet rs = null;
 
     private JPanel jContentPane = null;
     private JButton jButton = null;
@@ -24,17 +29,70 @@ public class AppWindow extends BaseWindow {
         if (jButton == null) {
             jButton = new JButton();
             // メインウインドウに対して、100x30 のボタンを追加
-            jButton.setBounds(
-                new Rectangle(
-                    (mainWidth/2)-50, ((mainHeight-40)/2)-15,
-                    100, 30
-            )
-            );
+            jButton.setBounds( 250, 30-7, 100, 30 );
             jButton.setText("実行");
             jButton.addActionListener(new java.awt.event.ActionListener() {
                 public void actionPerformed(java.awt.event.ActionEvent e) {
 
-                    MsgOk("こんにちは");
+                    try {
+                        conn = DriverManager.getConnection(
+                            "jdbc:mysql://localhost/lightbox?user=root&password=&characterEncoding=UTF-8"
+                        );
+
+                        // ステートメント
+                        stmt = conn.createStatement();
+                        // SQL 実行
+                        rs = stmt.executeQuery("select * from 社員マスタ where 社員コード = '0001'");
+
+                        // 一行取り出し
+                        rs.next();
+                        // 社員名
+                        String sName = rs.getString("氏名");
+                        MsgOk(sName);
+
+                        // 整数
+                        int seibetu = rs.getInt("性別");
+                        if ( seibetu == 0 ) {
+                            System.out.println("男性");
+                        }
+                        else {
+                            System.out.println("女性");
+                        }
+
+                        // 文字列
+                        String seibetu2 = String.format("%d", seibetu);
+                        if ( seibetu2.equals("0") ) {
+                            System.out.println("男性");
+                        }
+                        else {
+                            System.out.println("女性");
+                        }
+
+                        // while( rs.next() ) {
+
+                        //     System.out.println(rs.getString("社員コード"));
+                        //     System.out.println(rs.getString("氏名"));
+                        //     System.out.println(rs.getString("フリガナ"));
+                        //     System.out.println(rs.getString("所属"));
+                        //     System.out.println(rs.getString("性別"));
+                        //     System.out.println(rs.getString("作成日"));
+                        //     System.out.println(rs.getString("更新日"));
+                        //     System.out.println(rs.getString("給与"));
+                        //     System.out.println(rs.getString("手当"));
+                        //     System.out.println(rs.getString("管理者"));
+                        //     System.out.println(rs.getString("生年月日"));
+
+                        // }
+
+                        rs.close();
+                        stmt.close();
+                        conn.close();
+
+                    }
+                    catch (Exception ex) {
+                        System.out.println( ex.getMessage() );
+                        ex.printStackTrace();
+                    }
 
                 }
             });
